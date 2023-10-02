04da1e5e9e84877b8e3a6ab903e10e179268e9fe
noelwschneider <noelwschneider@gmail.com>
2023-10-02 14:33:21 -0500

    wrote challonge tournament post route

--- server/routes/challonge.router.js
+++ server/routes/challonge.router.js
@@ -4,14 +4,18 @@ const router = express.Router();
 const axios = require('axios');
 
 // Get tournament
-router.get('/', (req, res) => {
+router.get('/tournament', (req, res) => {
     const apiKey = 'qV7jr0n7GMbAB3kmSZt243vWb01Ak8rdHlfAVRdL';
     const tournamentURL = '9bugadqo'
     const endpointURL = `https://api.challonge.com/v1/tournaments/${tournamentURL}.json`;
 
     const fetchData = async () => {
         try {
-            let response = await axios.get(endpointURL, { params: { api_key: apiKey } });
+            let response = await axios.get(endpointURL, { 
+                params: { 
+                    api_key: apiKey 
+                } 
+            });
             console.log('response:', response.data);
         } catch (error) {
             console.error(error);
@@ -22,19 +26,31 @@ router.get('/', (req, res) => {
 });
 
 // Create tournament
-router.post('/', (req, res) => {
+router.post('/tournament', (req, res) => {
     
     const apiKey = process.env.CHALLONGE_API_KEY;
-    // Tournament object
-    const tournament = req.body;
+    const endpointURL = `https://api.challonge.com/v1/tournaments.json`;
     
-    axios.post('https://api.challonge.com/v1/tournaments.json', tournament)
-    .then( response => {
-        console.log('response:', response)
-    })
-    .catch( error => {
-        console.log('error in CREATE_TOURNAMENT', error)
-    })
+    const name = "test_tournament"
+    const createTournament = async () => {
+        try {
+            let response = await axios.post(endpointURL, { 
+                
+                    api_key: apiKey,
+                    name: name
+                
+            });
+            console.log('response:', response);
+            res.sendStatus(200)
+        } catch (error) {
+            console.error('error in tournament post:', error)
+            res.sendStatus(500)
+        }
+    }
+
+    createTournament();
+    
+    
 });
 
 module.exports = router;
