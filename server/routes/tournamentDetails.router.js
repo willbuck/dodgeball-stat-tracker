cfea4c82565233561001c47ca05525d2c14ece2e
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-10-02 15:54:14 -0500

    Created the  GET tournmentDetails have more information about the teams  playing

--- server/routes/tournamentDetails.router.js
+++ server/routes/tournamentDetails.router.js
@@ -6,9 +6,35 @@ router.get('/:id', (req,res) => {
     console.log('heyyyy')
     console.log('Params:', req.params.id)
 
-    const queryText = `-- to get all the games inside a specific tournament 
-    SELECT * FROM game
-    WHERE tournament_id = $1;`
+    const queryText = `
+    -- to get all the games inside a specific tournament 
+    --Also more details form the teams that are playing in that specific tournament
+    SELECT
+        game.id AS game_id,
+        game.team1_id AS team1_id,
+        t1.team_name AS team1_name,
+        t1.jersey_color AS team1_jersey_color,
+        game.team2_id AS team2_id,
+        t2.team_name AS team2_name,
+        t2.jersey_color AS team2_jersey_color,
+        game.start_time,
+        game.end_time,
+        game.date,
+        game.court,
+        game.team1_score,
+        game.team2_score,
+        game.game_status,
+        game.ball_type,
+        game.tournament_id
+    FROM
+        game 
+    JOIN
+        team t1 ON game.team1_id = t1.id
+    JOIN
+        team t2 ON game.team2_id = t2.id
+    WHERE
+        game.tournament_id = $1;
+    `
 
     const queryParams = [req.params.id];
 
@@ -17,6 +43,7 @@ router.get('/:id', (req,res) => {
             res.send(result.rows)
         }).catch((error) => {
             res.sendStatus(500);
+            console.log('Error with the GET_TournamentDetails', error);
         })
 })
 
