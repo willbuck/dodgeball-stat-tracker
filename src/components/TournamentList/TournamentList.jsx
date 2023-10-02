0d1b1b2307c3435d4c36ec36864831f3b8b8861b
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-10-02 15:25:42 -0500

    added link to each game in tournament list

--- src/components/TournamentList/TournamentList.jsx
+++ src/components/TournamentList/TournamentList.jsx
@@ -3,6 +3,8 @@ import Box from '@mui/material/Box';
 import Grid from '@mui/material/Unstable_Grid2';
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from 'react-router-dom/cjs/react-router-dom';
+
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
@@ -12,30 +14,39 @@ import Typography from '@mui/material/Typography';
 
 export default function TournamentList() {
 
-    const tournaments = useSelector((store) => store.tournamentReducer);
+  const history = useHistory();
+  const tournaments = useSelector((store) => store.tournamentReducer);
 
-    return (
-        <Box sx={{ display: 'flex', justifyContent: 'center' }}>  
-        <Box className="scroll-container"
+  const handleClick = (id) => {
+    history.push(`/tournamentDetails/${id}`)
+  }
 
-          sx={{
-            width: 255,
-            height: 500,
-            overflowY: "auto",
-            backgroundColor: 'primary.dark',
-            '&:hover': {
-              backgroundColor: 'primary.main',
-              opacity: [0.9, 0.8, 0.7],
-            },
-          }}
-        >
-            <Grid container sx={{ minWidth: 200, display: 'flex', justifyContent: 'center' }}
-            xs={12}
-            columnGap={6}
-            rowGap={2}>
+  return (
+    <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+      <Box className="scroll-container"
+
+        sx={{
+          width: 255,
+          height: 500,
+          overflowY: "auto",
+          backgroundColor: 'primary.dark',
+          '&:hover': {
+            backgroundColor: 'primary.main',
+            opacity: [0.9, 0.8, 0.7],
+          },
+        }}
+      >
+        <Grid container sx={{ minWidth: 200, display: 'flex', justifyContent: 'center' }}
+          xs={12}
+          columnGap={6}
+          rowGap={2}>
           {tournaments.map((tournament) => {
             return (
-                <Card sx={{ minWidth: 200, maxWidth: 250, display: 'flex', justifyContent: 'center' }}>
+              <Card
+                onClick={() => { handleClick(tournament.id) }}
+                key={tournament.id}
+                sx={{ minWidth: 200, maxWidth: 250, display: 'flex', justifyContent: 'center' }}
+              >
                 <CardContent>
                   <Typography sx={{ fontSize: 14 }} color='text.secondary' gutterBottom>
                     {tournament.date}
@@ -44,15 +55,15 @@ export default function TournamentList() {
                     {tournament.tournament_name} - Location: {tournament.location}
                   </Typography>
                 </CardContent>
-              </Card> 
+              </Card>
             )
           })}
-          </Grid>
-        </Box>
-        </Box>
-      );
-    }
+        </Grid>
+      </Box>
+    </Box>
+  );
+}
 
-    // <CardActions>
-    //               <Button size='small'>Learn More</Button>
-    //             </CardActions>
+// <CardActions>
+//               <Button size='small'>Learn More</Button>
+//             </CardActions>
