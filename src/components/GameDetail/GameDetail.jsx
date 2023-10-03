58955be8f9df326e8faadd664c8d502b9ceaf6fa
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-10-03 11:22:46 -0500

    started work on conditional header

---
+++ src/components/GameDetail/GameDetail.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+
+
+function GameDetail() {
+    const user = useSelector((store) => store.user);
+
+
+    return (
+        <div className="container">
+
+        </div>
+    );
+}
+
+export default GameDetail;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Link, useHistory, } from 'react-router-dom';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import { useSelector } from 'react-redux';
+// import { Height } from '@mui/icons-material';
+
+function Header() {
+    const history = useHistory();
+    const user = useSelector((store) => store.user);
+
+
+    const getCurrentRouteName = () => {
+        const pathname = history.location.pathname;
+
+        const routeName = pathname.split('/').filter(Boolean).pop();
+
+        return routeName;
+    };
+
+
+    const currentPage = getCurrentRouteName();
+
+
+    return (
+        <div className="header">
+            <Link to="/home">
+                <img
+                    src='https://s3.amazonaws.com/playpass-discovery/production/organizers/logos/31127/wide_USAD-SITELOGO.png?1581286742'
+                    style={{ height: '20px', width: '20px' }}
+                />
+            </Link>
+
+            <div>
+                <h2 className="header-title">{currentPage}</h2>
+            </div>
+
+            <div>
+                {/* If no user is logged in, show these links */}
+                {!user.id && (
+                    // If there's no user, show login/registration links
+                    <Link className="navLink" to="/login">
+                        Login / Register
+                    </Link>
+                )}
+
+                {/* If a user is logged in, show these links */}
+                {user.id && (
+                    <>
+                        <LogOutButton className="navLink" />
+                    </>
+                )}
+
+            </div>
+
+
+        </div>
+    );
+}
+
+export default Header;
