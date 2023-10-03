c924f6eb99a1064499c218cceeceb351d1959833
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-10-03 11:08:05 -0500

    HELPPPPPP

--- server/server.js
+++ server/server.js
@@ -11,12 +11,7 @@ const passport = require('./strategies/user.strategy');
 const userRouter = require('./routes/user.router');
 const challongeRouter = require('./routes/challonge.router')
 const tournamentRouter = require('./routes/tournament.router')
-<<<<<<< HEAD
 const tournamentDetailsRouter = require('./routes/tournamentDetails.router');
-=======
-
-
->>>>>>> 7ab938ede5328a398b01ddede67d65ed4c2eb088
 // Body parser middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -2,11 +2,7 @@ import { combineReducers } from 'redux';
 import errors from './errors.reducer';
 import user from './user.reducer';
 import tournamentReducer from './tournament.reducer';
-<<<<<<< HEAD
 import tournamentDetailsReducer from './tournamentDetails.reducer';
-=======
-import selectedTournamentReducer from './selectedtournament.reducer';
->>>>>>> 7ab938ede5328a398b01ddede67d65ed4c2eb088
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -18,12 +14,8 @@ const rootReducer = combineReducers({
   errors, // contains registrationMessage and loginMessage
   user, // will have an id and username if someone is logged in
   tournamentReducer,
-<<<<<<< HEAD
   tournamentDetailsReducer,
   
-=======
-  selectedTournamentReducer,
->>>>>>> 7ab938ede5328a398b01ddede67d65ed4c2eb088
 });
 
 export default rootReducer;
