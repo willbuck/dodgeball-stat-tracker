f3f897ada8ce7994b8ed31cc30e6b24765756343
Ben Lynch <bdlynch7@gmail.com>
2023-10-03 10:01:00 -0500

    refactored admin routes into app.js

--- src/components/Admin/AdminRoutes.jsx
+++ src/components/Admin/AdminRoutes.jsx
@@ -1,24 +1,12 @@
 import React, { useEffect } from 'react';
 
-import AdminLanding from './AdminLanding';
-import CreateTournament from './CreateTournament';
-import ProtectedRoute from '../ProtectedRoute/ProtectedRoute';
+
 
 
   function AdminRoutes() {
     return (
       <>
-        <ProtectedRoute exact path="/admin">
-          <AdminLanding />
-        </ProtectedRoute>
 
-        <ProtectedRoute exact path="/admin/create-tournament">
-          <CreateTournament />
-        </ProtectedRoute>
-        
-        <ProtectedRoute exact path="/admin/manage-tournament">
-          {/* Add your admin route components here */}
-        </ProtectedRoute>
 
         <ProtectedRoute exact path="/admin/route3">
           {/* Add your admin route components here */}
