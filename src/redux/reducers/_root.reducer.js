f4dad92a7664c447b7203acb6f95424baa95f1e0
Asiya  Mohamed <Asiya.mohamed256@gmail.com>
2023-10-03 11:17:17 -0500

    merging issues

--- src/redux/reducers/_root.reducer.js
+++ src/redux/reducers/_root.reducer.js
@@ -3,6 +3,7 @@ import errors from './errors.reducer';
 import user from './user.reducer';
 import tournamentReducer from './tournament.reducer';
 import tournamentDetailsReducer from './tournamentDetails.reducer';
+import selectedTournamentReducer from './selectedtournament.reducer';
 
 // rootReducer is the primary reducer for our entire project
 // It bundles up all of the other reducers so our project can use them.
@@ -15,7 +16,7 @@ const rootReducer = combineReducers({
   user, // will have an id and username if someone is logged in
   tournamentReducer,
   tournamentDetailsReducer,
-  
+  selectedTournamentReducer,
 });
 
 export default rootReducer;
