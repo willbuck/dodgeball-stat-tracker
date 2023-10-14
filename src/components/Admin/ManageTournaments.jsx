abe787d7320ae1a68bc5b43943ebdc9187beef8c
Samuel Nelson <samuel.h.nelson@protonmail.com>
2023-10-13 22:00:30 -0500

    converting samuel3 to samuel-finals

--- src/components/Admin/ManageTournaments.jsx
+++ src/components/Admin/ManageTournaments.jsx
@@ -1,12 +1,19 @@
 import React, { useState, useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
+import ManageTournamentsModal from './ManageTournamentsModal';
 import { Button, Box, Card, Typography, Grid, Container, Stack } from '@mui/material';
+
+import EditIcon from '@mui/icons-material/Edit';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 import { IconTrash } from '@tabler/icons-react';
+// import EditNoteIcon from '@mui/icons-material/EditNote';
 
 export default function ManageTournaments() {
 
   const dispatch = useDispatch()
   const allTeams = useSelector((store) => store.tournamentsReducer)
+  console.log('all teams', allTeams);
 
   //! Should this be async?
   const handleDelete = (id) => {
@@ -14,14 +21,30 @@ export default function ManageTournaments() {
     dispatch({ type: "FETCH_TOURNAMENTS" })
   }
 
-  // This is the basic framework - need to check all the variables 
+
+  // Edit Menu Functions
+  // anchorEl is the menu button's anchor (it cannot be renamed to propery function)
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+  const handleEdit = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+
+
+
   return (
     <Container>
       <Grid container spacing={3}>
         {allTeams.map((tournament) => (
+
+          // Maps Cards of Tournaments
           <Grid item xs={12} sm={6} md={4} key={tournament.id}>
 
-            {/*  */}
+            {/* Renders Card for Tournament*/}
             <Card sx={{ padding: '20px', margin: '10px', border: '1px solid grey' }}>
               <Box display="flex" justifyContent="space-between">
                 <Stack>
@@ -34,11 +57,45 @@ export default function ManageTournaments() {
                   </Typography>
                 </Stack>
 
-                <Button
-                  onClick={() => handleDelete(tournament.id)}
-                  color="secondary">
-                  <IconTrash size={24} />
-                </Button>
+
+
+                {/* Edit Button and Menu on Each Card*/}
+                <div>
+                  <Button
+                    id="basic-button"
+                    aria-controls={open ? 'basic-menu' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleEdit}
+
+                  >
+                    <EditIcon size={24} />
+                    Edit
+                  </Button>
+                  <Menu
+                    id="basic-menu"
+                    anchorEl={anchorEl}
+                    open={open}
+                    onClose={handleClose}
+                    MenuListProps={{
+                      'aria-labelledby': 'basic-button',
+                    }}
+                  >
+                    <MenuItem>
+                      <ManageTournamentsModal tournamentID={tournament.id} />
+                    </MenuItem>
+                    <MenuItem onClick={handleClose}>
+                      <Button
+                        onClick={() => handleDelete(tournament.id)}
+                      >
+                        <IconTrash size={20} />
+                        Delete
+                      </Button>
+                    </MenuItem>
+                  </Menu>
+                </div>
+
+
               </Box>
             </Card>
           </Grid>
